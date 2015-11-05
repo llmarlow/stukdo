8c72f8b0afc74d7f824dba7bb2bc27ddcfca0618
Lorna Marlow <bikergirl1515@hotmail.com>
2015-11-05 11:02:24 +0000

    Added validations to our task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105100906) do
+ActiveRecord::Schema.define(version: 20151105105811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 20151105100906) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "state",      default: "to_do"
+    t.string   "state",        default: "to_do"
+    t.text     "content_html"
   end
 
   create_table "users", force: true do |t|
